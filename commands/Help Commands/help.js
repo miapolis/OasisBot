b1bcb60ef0da041dee74075dc97ad32c78b91d20
Miapolis <elerner26@gmail.com>
2020-08-30 16:02:25 -0400

    Updated File

--- commands/Help Commands/help.js
+++ commands/Help Commands/help.js
@@ -1,5 +1,8 @@
+const path = require('path')
+const root = path.dirname(require.main.dirname)
+
 const discord = require("discord.js")
-const { prefix } = require('config.json');
+const { prefix } = require(root + 'config.json')
 
 const upperDescription = "View our [documentation](https://sites.google.com/view/oasisbot/documentation)\n" +
     "If you would like to send feedback or report a bug, fill out [this form](https://forms.gle/nwWKRLNJtqHW6Vfq9)\nIf you would like to further support this bot, specify **&donate**";
