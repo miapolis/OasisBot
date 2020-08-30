fbd3698e0e07ebe61699bca34bf6ebdb90abda97
Miapolis <elerner26@gmail.com>
2020-08-30 15:56:08 -0400

    Fixed?

--- commands/Help Commands/help.js
+++ commands/Help Commands/help.js
@@ -1,5 +1,5 @@
 const discord = require("discord.js")
-const { prefix } = require("../config.json")
+const { prefix } = require('config.json');
 
 const upperDescription = "View our [documentation](https://sites.google.com/view/oasisbot/documentation)\n" +
     "If you would like to send feedback or report a bug, fill out [this form](https://forms.gle/nwWKRLNJtqHW6Vfq9)\nIf you would like to further support this bot, specify **&donate**";
