78c050ef026004daf15b98f8b000d6cb43915585
Miapolis <elerner26@gmail.com>
2020-08-30 15:31:41 -0400

    Started Help Command

---
+++ commands/Help Commands/help.js
@@ -0,0 +1,17 @@
+const discord = require("discord.js")
+
+const upperDescription = "View our [documentation](https://sites.google.com/view/oasisbot/documentation)\n" +
+    "If you would like to send feedback or report a bug, fill out [this form](https://forms.gle/nwWKRLNJtqHW6Vfq9)\nIf you would like to further support this bot, specify **&donate**";
+
+module.exports = {
+    commands: 'help',
+    callback: (message, arguments, text) => {
+        let embed = new MessageEmbed()
+            .setTitle('Help')
+            .setDescription(
+                upperDescription + '\n\n' + 'HAHAH'
+            );
+
+        message.channel.sendMessage(embed);
+    }
+}
