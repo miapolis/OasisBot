7dbe6dd65015bb553d20a5b38611bd15033a83cb
Miapolis <elerner26@gmail.com>
2020-10-05 20:51:17 -0400

    Updated time for timezone differences

--- time-helper.js
+++ time-helper.js
@@ -2,7 +2,7 @@ module.exports.getFormattedMilitaryTime = () => {
     const today = new Date()
     const minutes = today.getMinutes().toString()
     const seconds = today.getSeconds().toString()
-    const time = `${today.getHours()}:${minutes.length === 1 ? '0' + minutes : minutes} - ${seconds.length === 1 ? '0' + seconds : seconds}s`
+    const time = `${today.getHours() - 6}:${minutes.length === 1 ? '0' + minutes : minutes} - ${seconds.length === 1 ? '0' + seconds : seconds}s`
     return time
 }
 
@@ -14,6 +14,6 @@ module.exports.getFormattedMilitaryTimeMill = () => {
 
     milliseconds = milliseconds.length === 1 ? '00' + milliseconds : milliseconds.length === 2 ? '0' + milliseconds : milliseconds
 
-    const time = `${today.getHours()}:${minutes.length === 1 ? '0' + minutes : minutes} - ${seconds.length === 1 ? '0' + seconds : seconds}.${milliseconds}s`
+    const time = `${today.getHours() - 6}:${minutes.length === 1 ? '0' + minutes : minutes} - ${seconds.length === 1 ? '0' + seconds : seconds}.${milliseconds}s`
     return time
 }
