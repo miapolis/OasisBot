ece4707608bcc8ed8083e50aa76fac0f532901a3
Miapolis <elerner26@gmail.com>
2020-10-05 20:59:27 -0400

    Fixed Timezone?

--- time-helper.js
+++ time-helper.js
@@ -1,19 +1,21 @@
 module.exports.getFormattedMilitaryTime = () => {
     const today = new Date()
+    const hours = today.getHours() - 6
     const minutes = today.getMinutes().toString()
     const seconds = today.getSeconds().toString()
-    const time = `${today.getHours() - 6}:${minutes.length === 1 ? '0' + minutes : minutes} - ${seconds.length === 1 ? '0' + seconds : seconds}s`
+    const time = `${hours}:${minutes.length === 1 ? '0' + minutes : minutes} - ${seconds.length === 1 ? '0' + seconds : seconds}s`
     return time
 }
 
 module.exports.getFormattedMilitaryTimeMill = () => {
     const today = new Date()
+    const hours = today.getHours() - 6
     const minutes = today.getMinutes().toString()
     const seconds = today.getSeconds().toString()
     let milliseconds = today.getMilliseconds().toString()
 
     milliseconds = milliseconds.length === 1 ? '00' + milliseconds : milliseconds.length === 2 ? '0' + milliseconds : milliseconds
 
-    const time = `${today.getHours() - 6}:${minutes.length === 1 ? '0' + minutes : minutes} - ${seconds.length === 1 ? '0' + seconds : seconds}.${milliseconds}s`
+    const time = `${hours}:${minutes.length === 1 ? '0' + minutes : minutes} - ${seconds.length === 1 ? '0' + seconds : seconds}.${milliseconds}s`
     return time
 }
